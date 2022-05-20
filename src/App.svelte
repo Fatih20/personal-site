4719e7a594fc0ae35a07878c16647fe39ab9c949
Fatih20 <fnri39@protonmail.com>
2022-05-20 13:05:14 +0700

    Splitting the components.

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,13 @@
 <script lang="ts">
+  import NameSection from "./components/NameSection.svelte";
 </script>
 
 <main>
-  <div class="header-content">
-    <h2>Hello! My name is</h2>
-    <h1>Fatih Nararya R. I.</h1>
-  </div>
+  <NameSection />
 </main>
 
 <style>
   main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-    /* border: solid 1px white; */
-  }
-
-  .header-content {
-    align-items: flex-start;
-    display: flex;
-    flex-direction: column;
-    gap: 1em;
-    justify-content: start;
-
-    /* border: solid 1px white; */
-  }
-
-  .header-content * {
-    text-align: left;
-  }
-
-  h1 {
-    font-size: 3em;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+    padding: 0 1em;
   }
 </style>
---
+++ src/components/NameSection.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+</script>
+
+<main>
+  <div class="header-content">
+    <h2>Hello! My name is</h2>
+    <h1>Fatih Nararya R. I.</h1>
+  </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    margin: 0 auto;
+    /* border: solid 1px white; */
+  }
+
+  .header-content {
+    align-items: flex-start;
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    justify-content: start;
+
+    /* border: solid 1px white; */
+  }
+
+  .header-content * {
+    text-align: left;
+  }
+
+  h1 {
+    font-size: 3em;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
