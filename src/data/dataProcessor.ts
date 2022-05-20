88312cfb4bb62eebea4fec43a7db5c9fc043d54c
Fatih20 <fnri39@protonmail.com>
2022-05-20 17:01:17 +0700

    Initalize the possible structure of data.

---
+++ src/data/dataProcessor.ts
@@ -0,0 +1,18 @@
+import { writable } from 'svelte/store';
+
+
+
+export const titleList = writable(["Aspiring Web Developer", "Student of STEI ITB"]);
+
+export const AchievementList = writable({
+    "Aspiring Web Developer" : [
+        {
+            
+        }
+    ],
+    "Student of STEI ITB" : [
+        {
+
+        }
+    ]
+})
